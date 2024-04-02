d7a583f651fd0b270e7006b9805627ef213a8593
fabiana <fabi_arza@hotmail.com>
2024-04-02 14:18:51 +0200

    Mi primer gran commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EmpleadosComponent } from './empleados/empleados.component';
+
+import { EmpleadoComponent } from "./empleado/empleado.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,EmpleadosComponent,EmpleadoComponent],
+ 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+//Esta es la clase y dentro creamos sus propiedades 
+//Estas propiedades luego puedo usarlos con etiquetas HTML  o solos
 export class AppComponent {
   title = 'miPrimeraAppAngular';
+  texto ='Estoy feliz de Aprender Angular v17'
 }
+//Video 7 Interpolacion de Strings
+
---
+++ src/app/empleados/empleados.component.ts
@@ -0,0 +1,30 @@
+import { Component } from "@angular/core";
+import { EmpleadoComponent } from "../empleado/empleado.component";
+
+
+
+
+@Component({
+    selector:"app-empleados",
+    standalone: true,
+    imports:[EmpleadoComponent],
+   
+    //templateUrl:"./empleados.component.html",
+    template:`
+    <h2>Empleados De La Empresa Componente Empleados</h2>
+    <p>Hola Angular</p>
+    `
+    ,   
+    
+   // styleUrl:"./empleados.component.css"
+   styles:["P{background:red; text-decoration:line-through;font-style:italic;}",
+   "h2{text-align: center;color:brown;text-shadow: 0px 1px 3px green;}"]
+
+
+
+})
+
+export class EmpleadosComponent {
+   
+
+}
